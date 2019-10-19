c5cb51f9ea6b0dab711b1c00da7c8ec79daf1a25
Iralitv <ira.litvinchuk@mifort.org>
2019-10-19 15:19:55 +0300

    feat: add collapsible list for education

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,15 @@
-import '../sass/styles.scss'
+import '../sass/styles.scss';
+
+const collapse = document.getElementsByClassName("education__collapse");
+
+for (let i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
+     this.classList.toggle("active");
+     let content = this.nextElementSibling;
+     if(content.style.maxHeight){
+       content.style.maxHeight = null;
+     } else {
+       content.style.maxHeight = content.scrollHeight + 'px';
+     }
+  });
+}
